1fa063fb07b74838f3f698c4d06773b6f69190f7
David Armstrong <wafflesultimate1@gmail.com>
2023-10-17 21:18:10 -0700

    Completed Exercise 2

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           List <string> videoGames = new List<string>() { "Valorant", "Mass Effect", "Stellaris", "Halo"};
+
+           var videoGamesOrdered = videoGames.OrderBy(x => x.Length);
+
+           foreach(var videoGame in videoGamesOrdered) { Console.WriteLine(videoGame); } 
+
         }
     }
 }
